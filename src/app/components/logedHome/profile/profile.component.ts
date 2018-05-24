1e97f50eeb36317ae48c805f3c2df95c7b85ed4e
Ismael Sayas Arrieta <ismael.sayas@PRAGMA.COM.CO>
2018-05-24 16:54:26 -0500

    My Profile, basic template

--- src/app/components/logedHome/profile/profile.component.ts
+++ src/app/components/logedHome/profile/profile.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-profile',
@@ -7,9 +8,38 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProfileComponent implements OnInit {
 
-  constructor() { }
+  private editUserForm: FormGroup;
+
+  constructor(
+    private buildForm: FormBuilder
+  ) { }
 
   ngOnInit() {
+    this.editUserForm = this.buildForm.group({
+      user: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      pass: ['', [Validators.required, Validators.minLength(6)]],
+      pass2: ['', [Validators.required, Validators.minLength(6)]]
+    })
   }
+
+  //==========================================================
+  // Local methods.
+  //==========================================================
+
+  private isPasswordsMatch() {
+    let userData = this.editUserForm.value;
+
+    return userData.pass === userData.pass2;
+  }
+
+  //==========================================================
+  // Getters.
+  //==========================================================
+
+  private get user() { return this.editUserForm.get('user'); }
+  private get email() { return this.editUserForm.get('email'); }
+  private get pass() { return this.editUserForm.get('pass'); }
+  private get pass2() { return this.editUserForm.get('pass2'); }
 
 }
